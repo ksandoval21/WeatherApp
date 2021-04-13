e2e73df08c7d72dc31a130074f2d7141ef04bb63
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-04-13 10:50:13 -0500

    my genius frightens me sometimes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+city= "Memphis"
+
+var url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=35f1b4967b15466c87502e7b3aef9681`
+
+router.get('/heyman', function(req, res, next){
+  console.log("pain")
+  request(url, function(error, response, body){
+    console.log("wassup dude")
+    weather_json= JSON.parse(body);
+    var weather = {
+      city : city,
+      temperature : (1.8 * (Math.round(weather_json.main.temp)- 273) + 32),
+      description : weather_json.weather[0].description,
+      icon : weather_json.weather[0].icon
+    };
+
+    var weather_data = {weather : weather};
+    res.render("hey", weather_data)
+    
+
+  })
+})
+
 
 module.exports = router;
