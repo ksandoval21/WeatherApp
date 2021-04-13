662747f5f2bd8e26f914e5edbf29a19bcc31696f
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2021-04-13 16:20:41 -0500

    worked on the css

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jade', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,4 +39,17 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
+city= "Memphis"
+var url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=35f1b4967b15466c87502e7b3aef9681`
+app.get('/', function(req, res){
+  request(url, function(error, response, body){
+    weather_json= JSON.parse(body)
+    var weather ={
+      city: city,
+      tempurature: weather_json.main.temp,
+      description: weather_json.weather[0].description
+    }
+    var weather_data={weather: weather}
+    res.render('views/layout.jade', weather_data)
+  })
+})
