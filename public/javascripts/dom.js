d6a69f680c87b49c27418ee838be013dec2ff6cc
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-04-14 15:07:04 -0500

    i might be close

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -1,26 +1,23 @@
-var request = require('request');
-const city = document.querySelector("#input")
-const btn = document.querySelector("#btn")
-console.log("Wassup my guy")
-var url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=35f1b4967b15466c87502e7b3aef9681`
+const cityBtn = document.getElementById("cityButton")
+const city = document.getElementById("input")
 
-
-
-request(url, function(error, response, body){
-    console.log("wassup dude")
-    weather_json= JSON.parse(body);
-    var weather = {
+cityBtn.addEventListener("click", () => {
+    var url= `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=35f1b4967b15466c87502e7b3aef9681`
+    fetch(url)
+    .then(response => response.json())
+    .then(weather = {
         city : city,
-        temperature :  parseInt(1.8 * (Math.round(weather_json.main.temp)- 273) + 32),
-        description : weather_json.weather[0].description,
-        icon : weather_json.weather[0].icon
-    };
-    console.log("infinite")
-})
-console.log("sup dude")
-var weather_data = {weather : weather};
-
-
-btn.addEventListener("click", () => {
+        temperature :  parseInt(1.8 * (Math.round(response.main.temp)- 273) + 32),
+        description : response.weather[0].description,
+        icon : response.weather[0].icon
+    })
     console.log(city.value)
 })
+
+
+    
+
+
+// console.log("infinite")
+    // console.log("sup dude")
+    // var weather_data = {weather : weather};
