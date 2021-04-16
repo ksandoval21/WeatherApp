7c3e2ef7e54d4a062f543475dfffc3c9985a14d5
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-04-16 09:36:24 -0500

    adding favorites

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -4,7 +4,6 @@ const city = document.getElementById("city")
 const temp = document.getElementById("temperature")
 const description = document.getElementById("description")
 const icon = document.getElementById("icon")
-let stateCode 
 const stateInput = document.getElementById("stateInput")
 const star = document.getElementById("star")
 const container = document.querySelector(".container")
@@ -15,7 +14,6 @@ cityBtn.addEventListener("click", () => {
     var url= `https://api.openweathermap.org/data/2.5/weather?zip=${cityInput.value},US&appid=35f1b4967b15466c87502e7b3aef9681`
     container.style.visibility ="visible";
     title.style.visibility ="visible";
-    console.log(url)
     fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -24,24 +22,30 @@ cityBtn.addEventListener("click", () => {
         temperature :  parseInt((data.main.temp- 273.15) * 1.8 + 32),
         description : data.weather[0].description,
         icon : data.weather[0].icon
-    };
-    city.innerHTML = weather.city
-    temp.innerHTML = weather.temperature +" °F"
-    description.innerHTML = weather.description
-    icon.src = `http://openweathermap.org/img/w/${weather.icon}.png`;
-   
-
+        };
+        city.innerHTML = weather.city
+        temp.innerHTML = weather.temperature +" °F"
+        description.innerHTML = weather.description
+        icon.src = `http://openweathermap.org/img/w/${weather.icon}.png`;
+    });
 });
-
-
-})
 
 let display= false
-star.addEventListener("click", () => {
-if(display){
-    star.classList.add("checked")
-}else{
-    star.classList.remove("checked")
+// star.addEventListener("click", () => {
+// if(display){
+//     star.classList.add("checked")
+// }else{
+//     star.classList.remove("checked")
+// }
+// display = !display
+// });
+
+
+
+for (const place of favorites){
+    if (place.class === "unchecked"){
+        const index = favorites.indexOf(place);
+        favorites.splice(index, 1)
+    }
 }
-display = !display
-});
+console.log(favorites)
