fbcc81d1fab577535bcd5a70e71118ed46b97914
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-04-16 10:02:11 -0500

    progress stops

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -8,7 +8,8 @@ const stateInput = document.getElementById("stateInput")
 const star = document.getElementById("star")
 const container = document.querySelector(".container")
 const title = document.querySelector(".title")
-const favorites = document.querySelector(".favorites")
+const favoritesContainer = document.querySelector(".favorites")
+let favorites = []
 
 cityBtn.addEventListener("click", () => {
     var url= `https://api.openweathermap.org/data/2.5/weather?zip=${cityInput.value},US&appid=35f1b4967b15466c87502e7b3aef9681`
@@ -21,48 +22,51 @@ cityBtn.addEventListener("click", () => {
         city : data.name,
         temperature :  parseInt((data.main.temp- 273.15) * 1.8 + 32),
         description : data.weather[0].description,
-        icon : data.weather[0].icon
-<<<<<<< HEAD
+        icon : data.weather[0].icon,
+        class : "unchecked"
     };
     city.innerHTML = weather.city
     temp.innerHTML = weather.temperature +" °F"
     description.innerHTML = weather.description
     icon.src = `http://openweathermap.org/img/w/${weather.icon}.png`;
-=======
-        };
-        city.innerHTML = weather.city
-        temp.innerHTML = weather.temperature +" °F"
-        description.innerHTML = weather.description
-        icon.src = `http://openweathermap.org/img/w/${weather.icon}.png`;
+
+
+    let display = false
+    star.addEventListener("click", () => {
+    if(display){
+        star.classList.add("checked")
+        // favorites.push(weather = {
+        //     city : data.name,
+        //     temperature :  parseInt((data.main.temp- 273.15) * 1.8 + 32),
+        //     description : data.weather[0].description,
+        //     icon : data.weather[0].icon,
+        //     class : "checked"
+        // })
+        // console.log(favorites)
+
+    }else{
+        star.classList.remove("checked")
+    //     for (const place of favorites){
+    //         if (place.weather.class === "unchecked"){
+    //             const index = favorites.indexOf(place);
+    //             favorites.splice(index, 1)
+    //         }
+    //     }
+    // }
+    display = !display
+
     });
->>>>>>> 38dd2181c5f7814325c740fd01d8ef1a5e890c56
+
+
+})
 });
 
-let display= false
-// star.addEventListener("click", () => {
-// if(display){
-//     star.classList.add("checked")
-// }else{
-//     star.classList.remove("checked")
-// }
-// display = !display
-// });
 
 
-let display= false
-star.addEventListener("click", () => {
-if(display){
-    star.classList.add("checked")
 
-}else{
-    star.classList.remove("checked")
-}
-display = !display
-});
-// let favorites = []
 // function displayFavorites(favorites){
 //     for (const favorite of favorites) {
-//         favorites.insertAdjacentHTML(
+//         favoritesContainer.insertAdjacentHTML(
 //         "beforeend",`
 //         <div class="container">
 //             <p class="city" ></p>
@@ -71,13 +75,8 @@ display = !display
 //             <p></p>
 //         </div>`
 //         )
-//     }
+//     };
 // }
 
-for (const place of favorites){
-    if (place.class === "unchecked"){
-        const index = favorites.indexOf(place);
-        favorites.splice(index, 1)
-    }
-}
-console.log(favorites)
+// displayFavorites(favorites)
+// console.log(favorites)
